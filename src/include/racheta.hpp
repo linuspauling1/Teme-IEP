25169752607d8fa41f283d0fa40e888be43c223e
VladuxRTK <33584762+VladuxRTK@users.noreply.github.com>
2021-11-25 14:33:30 +0200

    Am adaugat header-ul pentru biblioteca Racheta.

--- src/include/racheta.hpp
+++ src/include/racheta.hpp
@@ -1,1 +1,32 @@
 #pragma once
+
+class Racheta{
+	private:
+		int size_serie;//dimensiunea seriei rachetei
+		char* serie;//seria rachetei
+		short componente;
+		long double carburant;
+		int astronauti;
+	public:
+		Racheta();//default constructor
+		Racheta(int,char*,short,long double,int);
+		Racheta(const Racheta&);//copy constructor
+		Racheta(Racheta&&);//move constructor
+		~Racheta();//destructor
+		Racheta& operator=(const Racheta&);//copy assignment operator
+		Racheta& operator=(Racheta&&);//move assignment operator
+		void afiseaza();
+		//getter-e si setter-e
+		//setter-e:
+		void setSize_serie(int);
+		void setSerie(char*);
+		void setComponente(short);
+		void setCarburant(long double);
+		void setAstronauti(int);
+		//getter-e:
+		int getSize_serie();
+		char* getSerie();
+		short getComponente();
+		long double getCarburant();
+		int getAstronauti();
+}
