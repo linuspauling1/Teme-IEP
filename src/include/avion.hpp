47737618473f7a746acee2d546fed8f910c83400
VladuxRTK <33584762+VladuxRTK@users.noreply.github.com>
2021-11-25 14:32:14 +0200

    Am adaugat header-ul pentru biblioteca Avion.

--- src/include/avion.hpp
+++ src/include/avion.hpp
@@ -1,1 +1,32 @@
 #pragma once
+
+class Avion{
+	private:
+		int size_nume;//dimensiunea numelui
+		char* nume;//numele avionului
+		int motoare;
+		float consum;
+		int pasageri;
+	public:
+		Avion();//default constructor
+		Avion(int size_nume,char* nume,int motoare, float consum,int pasageri);//constructor
+		Avion(const Avion&);//copy constrcutor
+		Avion(Avion&&);//move constructor
+		~Avion();//destructor
+		Avion& operator=(const Avion&);//copy assignment operator
+		Avion& operator=(Avion&&);//move assignment operator
+		void afiseaza();
+		//getter-e si setter-e
+		//setter-e:
+		void setSize_nume(int size_nume);
+		void setNume(char* nume);
+		void setMotoare(int motoare);
+		void setConsum(float consum);
+		void setPasageri(int pasageri);
+		//getter-e:
+		int getSize_nume();
+		char* getNume();
+		int getMotoare();
+		float getConsum();
+		int getPasageri();
+}
