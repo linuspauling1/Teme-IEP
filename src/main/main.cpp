4b859ae9d37f4078ee784169deb288de5bd46444
VladuxRTK <33584762+VladuxRTK@users.noreply.github.com>
2021-11-26 16:18:06 +0200

    Am adaugat fisierul <<main.cpp>>.

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -2,7 +2,41 @@
 #include "avion.hpp"
 #include "racheta.hpp"
 #include "hovercraft.hpp"
+#include <utility>
 
 int main(){
+	Avion a;//constructor default
+	a.afiseaza();
+	Avion b(5,(char*)"B777",2,10000.08,300);//constructor
+	b.afiseaza();
+	Avion c = a;//copy constructor
+	c.afiseaza();
+	c.setMotoare(4);
+	c.afiseaza();
+	a.afiseaza();
+	c = b;//copy assignment operator
+	c.afiseaza();
+	c.setMotoare(3);
+	c.afiseaza();
+	b.afiseaza();
+	Avion d = Avion();//move constructor
+	d.afiseaza();
+	c = std::move(a);//move assignment operator
+	a.afiseaza();//variabila a fost distrusa in constructorul lui c
+	c.afiseaza();//ar trebui sa fie identic cu continutul lui a
+	/*--------------------------------------------------------------*/
+	Racheta w;//constructor default
+	w.afiseaza();
+	Racheta x(5,(char*)"ALLL",3,14003.45,14);//constructor
+	x.afiseaza();
+	Racheta y = w;//copy constructor
+	y.afiseaza();
+	y = x;//copy assignemnt operator
+	y.afiseaza();
+	Racheta z;
+	z = Racheta();//move constructor
+	z.afiseaza();
+	y = std::move(w);//move assignment operator
+	y.afiseaza();
 	return 0;
 }
